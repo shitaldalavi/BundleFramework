d1d46540952fc611a3bc1440e47da84b2bc8b984
shital.dalavi <shital.dalavi@nciportal.com>
2021-08-05 11:56:10 +0530

    First commit

---
+++ BundleFramework/BundleFramework/BundleFramework.h
@@ -0,0 +1,18 @@
+//
+//  BundleFramework.h
+//  BundleFramework
+//
+//  Created by admin on 05/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BundleFramework.
+FOUNDATION_EXPORT double BundleFrameworkVersionNumber;
+
+//! Project version string for BundleFramework.
+FOUNDATION_EXPORT const unsigned char BundleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BundleFramework/PublicHeader.h>
+
+
